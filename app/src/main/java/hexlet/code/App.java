daf5eefa048ddfddbf56455dce9e0829097b3630
Vildan <vildan124@yandex.ru>
2025-02-04 15:05:06 +0500

    add Makefile and Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,9 @@
 package hexlet.code;
-
-public class App {
+ class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Cli cli = new Cli();
+        Cli.read();
+        System.out.println("Hello, " + Cli.getName() + "!");
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String name;
+
+
+    public static void read() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I Have your name?");
+        name = sc.nextLine();
+    }
+    public static String getName(){
+        return name;
+    }
+}
